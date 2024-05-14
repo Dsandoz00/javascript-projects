4435898438860609b09b4c1fda7fc6927ee29e0d
Dsandoz00 <dxs.creatives@gmail.com>
2024-05-14 10:56:39 -0500

    Ch.9 Repeating with Loops

--- loops/chapter-examples/Loop-Variable.js
+++ loops/chapter-examples/Loop-Variable.js
@@ -1,5 +1,9 @@
 // Experiment with this loop by modifying each of the following: the variable initialization, the boolean condition, and the update expression.
 
-for (let i = 0; i < 51; i++) {
+for (let i = 5; i < 79; i++) {
    console.log(i);
+ }
+
+ for (let i = 25; i < 100; i++) {
+  console.log(i)
  }
--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,9 @@
 // create an array variable containing the names
 
+let fruits = ["Banana", "Dragonfruit", "Kiwi", "Mango"];
+
 // write a for loop that prints each name on a different line
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
