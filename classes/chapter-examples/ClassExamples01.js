ff7324f116a3c04867ad31a1e416bcb6c1c67ba5
Dsandoz00 <dxs.creatives@gmail.com>
2024-07-13 20:03:29 -0500

    Html

--- classes/chapter-examples/ClassExamples01.js
+++ classes/chapter-examples/ClassExamples01.js
@@ -1,13 +1,14 @@
 //Try adding new properties inside constructor.
 class Astronaut {
-   constructor(name, age, mass){
+   constructor(name, nickname, age, mass,){
      this.name = name;
      this.age = age;
      this.mass = mass;
+     this.nickname = nickname;
    }
 }
 
-let fox = new Astronaut('Fox', 7, 12);
+let fox = new Astronaut('Fox', 7, 12, 'Foxy Mama');
 
 console.log(fox);
 console.log(fox.age, fox.color);
@@ -16,6 +17,9 @@ fox.age = 9;
 fox.color = 'red';
 
 console.log(fox);
-console.log(fox.age, fox.color);
+console.log(fox.age, fox.color, fox.nickname);
 
 //Try modifying or adding properties below.
+
+fox.favoriteColor = 'blue';
+console.log(fox.nickname, fox.favoriteColor);
--- classes/chapter-examples/ClassExamples02.js
+++ classes/chapter-examples/ClassExamples02.js
@@ -3,7 +3,7 @@
 // Next, set default values for 1 or more of the parameters in constructor.
 
 class Astronaut {
-   constructor(name, age, mass){
+   constructor(name, age = 56, mass = 69){
      this.name = name;
      this.age = age;
      this.mass = mass;
